d76fd315e8bef1bc39ed7e35fa33ebefb25d3c39
abe555 <riho.has3v@gmail.com>
2020-07-06 12:21:45 +0900

    accomplish exercise

--- 2_Ruby/2-8/lesson8_2.rb
+++ 2_Ruby/2-8/lesson8_2.rb
@@ -1,3 +1,27 @@
 puts "計算をはじめます"
+puts "何回繰り返しますか？"
 
+puts
+inputs_key = gets.to_i
+
+number = 0
+while number < inputs_key do
+
+	number = number + 1
+
+	puts "#{number}回目の計算"
+	puts "2つの値を入力してください"
+
+	gets
+	a=gets.to_i
+	b=gets.to_i
+
+	puts "a+b=#{a+b}"
+	puts "a-b=#{a-b}"
+	puts "a*b=#{a*b}"
+	puts "a/b=#{a/b}"
+
+end
+
+puts "計算を終了します"
 
