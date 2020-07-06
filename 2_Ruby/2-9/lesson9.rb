20a8ea1ff47b9e137ce4e97c8d0c2307ba85821b
abe555 <riho.has3v@gmail.com>
2020-07-06 15:59:19 +0900

    Add if-content

---
+++ 2_Ruby/2-9/lesson9.rb
@@ -0,0 +1,13 @@
+puts
+
+number = gets.to_i
+
+if number % 3 == 0 && number % 5 == 0
+	puts "FizzBuzz"
+elsif number % 3 == 0
+	puts "Fizz"
+elsif number % 5 == 0
+	puts "Buzz"
+else
+	puts number.to_s
+end
